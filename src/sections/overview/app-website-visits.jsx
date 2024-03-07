470051bf45ad869fed4a69f55b4daaba0558b552
DonovanPS <91863877+srr0bot@users.noreply.github.com>
2024-03-07 10:56:35 -0500

    mostrar totales en ventas por iva

--- src/sections/overview/app-website-visits.jsx
+++ src/sections/overview/app-website-visits.jsx
@@ -2,13 +2,14 @@ import PropTypes from 'prop-types';
 
 import Box from '@mui/material/Box';
 import Card from '@mui/material/Card';
+import { Button } from '@mui/material';
 import CardHeader from '@mui/material/CardHeader';
 
 import Chart, { useChart } from 'src/components/chart';
 
 // ----------------------------------------------------------------------
 
-export default function AppWebsiteVisits({ title, subheader, chart, ...other }) {
+export default function AppWebsiteVisits({ title, subheader, subheader2, chart, ...other }) {
   const { labels, colors, series, options } = chart;
 
   const chartOptions = useChart({
@@ -42,13 +43,31 @@ export default function AppWebsiteVisits({ title, subheader, chart, ...other }) 
 
   return (
     <Card {...other}>
-      <CardHeader title={title} subheader={subheader} />
+      <CardHeader
+        title={title}
+        subheader={
+          <>
+            <div>{subheader}</div>
+            <div>{subheader2}</div>
+          </>
+        }
+        action={
+          <Button aria-label="Ver en factura">
+            Ver en factura
+          </Button>
+        }
+      />
 
       <Box sx={{ p: 3, pb: 1 }}>
         <Chart
           dir="ltr"
           type="line"
-          series={series}
+          series={series.map((item) => ({
+            name: ` ${item.name} - Productos: ${item.totalProducts} - $${item.totalPrice}`, // Nombre de la serie
+            data: item.data, // Datos de la serie
+            fill: item.fill, // Tipo de relleno de la serie
+            type: item.type, // Tipo de gráfico de la serie
+          }))}
           options={chartOptions}
           width="100%"
           height={364}
@@ -61,5 +80,6 @@ export default function AppWebsiteVisits({ title, subheader, chart, ...other }) 
 AppWebsiteVisits.propTypes = {
   chart: PropTypes.object,
   subheader: PropTypes.string,
+  subheader2: PropTypes.string,
   title: PropTypes.string,
 };
